c962e9325eeae4537394a832ef3419d697fe01f1
glowingboy <glowingboy@outlook.com>
2017-08-18 18:36:54 +0800

    ThreadPool added

---
+++ ThreadPool/gbThreadPool.cpp
@@ -0,0 +1,61 @@
+#include "gbThreadPool.h"
+#include "Log/gbLog.h"
+#include "String/gbString.h"
+
+gbJob::gbJob(std::function<void(void*)> func, void* arg, Priority p):
+	_p(p),
+	_bindfunc(std::bind(func, arg))
+{
+}
+
+std::list<std::thread*> gbThreadPool::_lstFreeThreads;
+std::condition_variable gbThreadPool::_cv;
+std::mutex gbThreadPool::_cv_m;
+std::priority_queue<gbJob> gbThreadPool::_jobs;
+
+bool gbThreadPool::Initialize(const int threadCount)
+{
+	int count = threadCount < 0 ? std::thread::hardware_concurrency() : threadCount;
+	gbLog::Instance().Log(gbString("threadCount: ") + count);
+	for (int i = 0; i < count; i++)
+	{
+		std::thread* t = new std::thread(_infinite_loop);
+		if (t == nullptr)
+			return false;
+	}
+
+	return true;
+}
+
+void gbThreadPool::_infinite_loop()
+{
+	while (true)
+	{
+		std::unique_lock<std::mutex> m(_cv_m);
+		if (_jobs.size() > 0)
+		{
+			gbLog::Instance().Log("i am woken up now @"); std::cout << std::this_thread::get_id() << std::endl;
+
+			gbJob job;
+			{
+				job = _jobs.top();
+				_jobs.pop();
+			}
+			m.unlock();
+
+			job.Do();
+		}
+		else
+			_cv.wait(m);
+	}
+}
+
+void gbThreadPool::PushJob(const gbJob& job)
+{
+	{
+		std::lock_guard<std::mutex> lg(_cv_m);
+		_jobs.push(job);
+	}
+
+	_cv.notify_one();
+}
---
+++ ThreadPool/gbThreadPool.h
@@ -0,0 +1,40 @@
+#pragma once 
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+#include <list>
+#include "gbCommon.h"
+class gbJob
+{
+	enum Priority
+	{
+		low = 1, mid, high
+	};
+public:
+	inline gbJob() {}
+	gbJob(std::function<void(void*)> func, void* arg, Priority p = Priority::mid);
+	inline bool operator<(const gbJob& other)const { return _p < other._p; }
+	inline void Do() { _bindfunc(); }
+private:
+	std::function<void()> _bindfunc;
+	Priority _p;
+
+};
+class gbThreadPool
+{
+	SingletonDeclare(gbThreadPool);
+
+public:
+	//if threadCount < 0, then using core's num
+	bool Initialize(const int threadCount);
+	void PushJob(const gbJob& job);
+private:
+	static std::list<std::thread*> _lstFreeThreads;
+	static std::condition_variable _cv;
+	static std::mutex _cv_m;
+	static std::priority_queue<gbJob> _jobs;
+private:
+	static void _infinite_loop();
+};
