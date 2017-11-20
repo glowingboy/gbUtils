953c71fef25e7154679706e953b6c100058de6fa
glowingboy <glowingboy@outlook.com>
2017-11-20 18:49:16 +0800

    gbCMake updated

--- test/test.cpp
+++ test/test.cpp
@@ -5,9 +5,11 @@
 #include "logger_test.cpp"
 #include "filesystem_test.cpp"
 
-#define test(test_func, ...)				\
-    if(test_func(__VA_ARGS__) != 0)			\
-	std::cout << #test_func << " failed" << std::endl;
+#define test(test_func, ...)					\
+    if(test_func(__VA_ARGS__) == 0)				\
+	std::cout << #test_func << " succeeded" << std::endl;	\
+    else							\
+	std::cout << #test_func << " failed" << std::endl; 
 
 int main(int argc, char** argv)
 {
