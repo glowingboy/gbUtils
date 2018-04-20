1e5015437bfa1154be466394db5f02d166461fc0
glowingboy <glowingboy@outlook.com>
2018-04-20 13:05:06 +0800

    compiling error fixed

--- src/callback.h
+++ src/callback.h
@@ -3,6 +3,7 @@
 #include "ns.h"
 #include <functional>
 #include <unordered_map>
+#include <algorithm>
 
 #include "string.h"
 #include "logger.h"
@@ -10,6 +11,12 @@ GB_UTILS_NS_BEGIN
 
 #define GB_UTILS_CALLBACK_REG(cbObj, memFunc, ...)			\
     cbObj.RegisterCB(#memFunc, (std::uintptr_t)this, std::bind(&memFunc, this, __VA_ARGS__));
+
+#define GB_UTILS_CALLBACK_REG_1P(cbObj, memFunc)	\
+    GB_UTILS_CALLBACK_REG(cbObj, memFunc, std::placeholders::_1)
+
+#define GB_UTILS_CALLBACK_REG_2P(cbObj, memFunc)	\
+    GB_UTILS_CALLBACK_REG(cbObj, memFunc, std::placeholders::_1, std::placeholders::_2)
 
 #define GB_UTILS_CALLBACK_UNREG(cbObj, memFunc)		\
     cbObj.UnregisterCB(#memFunc, (std::uintptr_t)this);
@@ -30,18 +37,18 @@ public:
 		    const std::uintptr_t instanceAddr,
 		    const std::function<void(FuncParams ...)>& cb)
 	{
-	    auto funcs = _mpCBS.find(szFuncName);
+	    auto funcs = _mpCBs.find(szFuncName);
 	    if(funcs != _mpCBs.end())
 	    {
 		auto ret = funcs->second.insert(std::make_pair(instanceAddr, cb));
 		if(! ret.second)
-		    logger::Instance().warning(string("callback::RegisterCB failed szFuncName@ "
+		    logger::Instance().warning(string("callback::RegisterCB failed szFuncName@ ")
 						      + szFuncName
-						      + ", instanceAddr@ " + instanceAddr));
+						      + ", instanceAddr@ " + instanceAddr);
 	    }
 	    else
 		_mpCBs.insert(std::make_pair(szFuncName,
-					     std::unordered_map<std::uintptr_t, std::function<void(FuncParams ...)
+					     std::unordered_map<std::uintptr_t, std::function<void(FuncParams ...)>>
 					     {{instanceAddr, cb}}));
 	}
     
@@ -56,14 +63,14 @@ public:
 
     void Trigger(FuncParams ... func_params)
 	{
-	    std::for_each(_mpCBs.begin(), _mpCBs.end(), [](const std::pair<string,
+	    std::for_each(_mpCBs.begin(), _mpCBs.end(), [&func_params ...](const std::pair<string,
 							   std::unordered_map<std::uintptr_t, std::function<void(FuncParams ...)>>>& funcs)
 			  {
 			      const auto func = funcs.second;
 
-			      std::for_each(func.begin(), func.end(), [&func_params ...](const std::pair<std::uintptr_t, std::function<void(FuncParams ...)>& f)
+			      std::for_each(func.begin(), func.end(), [&func_params ...](const std::pair<std::uintptr_t, std::function<void(FuncParams ...)>>& f)
 					    {
-						f(func_params ...);
+						f.second(func_params ...);
 					    });
 			  });
 	}
@@ -73,6 +80,12 @@ private:
 
 #define GB_UTILS_MULTI_CALLBACK_REG(cbObj, triggerKey, memFunc, ...)	\
     cbObj.RegisterCB(triggerKey, #memFunc, (std::uintptr_t)this, std::bind(&memFunc, this, __VA_ARGS__));
+
+#define GB_UTILS_MULTI_CALLBACK_REG_1P(cbObj, triggerKey, memFunc)	\
+    GB_UTILS_MULTI_CALLBACK_REG(cbObj, triggerKey, memFunc, std::placeholders::_1);
+
+#define GB_UTILS_MULTI_CALLBACK_REG_2P(cbObj, triggerKey, memFunc)	\
+    GB_UTILS_MULTI_CALLBACK_REG(cbObj, triggerKey, memFunc, std::placeholders::_1, std::Placeholders::_2);
 
 #define GB_UTILS_MULTI_CALLBACK_UNREG(cbObj, triggerKey, memFunc)	\
     cbObj.UnregisterCB(triggerKey, #memFunc, (std::uintptr_t)this);
